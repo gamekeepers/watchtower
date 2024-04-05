51fcd6c5d9e9c2ad91d1a4edc26aad427a180174
Ankush Chander <ankush.watchtower@gmail.com>
2024-04-05 16:23:07 +0530

    add screenshot on readme

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,7 +2,7 @@
 export const Header = () => (
   <div className="flex flex-row justify-between space-x-6 px-8 py-3.5 bg-black w-full">
     <div className="pr-8 border-r border-ink">
-      <a href="/">
+      <a href="/" className="text-white">
         Watchtower
       </a>
     </div>
